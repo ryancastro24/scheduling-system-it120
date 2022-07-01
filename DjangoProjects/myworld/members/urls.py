a1782ff622edc79baf4ad2c4ec660abe1be9ed43
Ryan Joel <ryanjoelimba@gmail.com>
2022-07-01 23:10:28 +0800

    added required attribute for input fields

--- DjangoProjects/myworld/members/urls.py
+++ DjangoProjects/myworld/members/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path('updatePage/',views.updatePage, name='updatePage'),
     path('updatePage/update/<int:id>',views.update, name='update'),
     path('updatePage/update/updaterecord/<int:id>',views.updaterecord, name='updaterecord'),
-
 ]
